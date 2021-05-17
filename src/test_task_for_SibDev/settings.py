be4888b9c9a53ce010c98a193a854c313837d2d1
lelyavin_907 <nlelyavin1@yandex.ru>
2021-05-18 01:23:39 +0300

    settings of docker

--- src/test_task_for_SibDev/settings.py
+++ src/test_task_for_SibDev/settings.py
@@ -73,15 +73,22 @@ WSGI_APPLICATION = 'test_task_for_SibDev.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', 'sibdev'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'zinnurik123321'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': 'sibdev',
+        'USER': 'postgres',
+        'PASSWORD': 'zinnurik123321',
+        'HOST': '192.168.99.100',
+        'PORT': '5432',
 
     }
 }
